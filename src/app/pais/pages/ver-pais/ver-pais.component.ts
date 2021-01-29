cf88e852b01137cb74a22f788d5f5167e3f062e6
eniliosarcos <eniliosarcos1993@gmail.com>
2021-01-29 12:22:01 -0400

    first commit

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators'
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private paisSerice: PaisService) { }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params
+        .pipe(
+          switchMap(({id}) => this.paisSerice.getPaisPorAplha(id)),
+          tap(console.log)
+        )
+        .subscribe(pais => this.pais = pais);
+          // console.log();
+
+    // this.activatedRoute.params
+    //     .subscribe(({id}) =>{
+    //       console.log(id);
+
+    //       this.paisSerice.getPaisPorAplha(id)
+    //       .subscribe(pais => {
+    //         console.log(pais);
+    //       });
+    //     });
+  }
+
+}
