f440782e3f32004661e16a9ef3edddf29b3a66a1
eniliosarcos <eniliosarcosdev@gmail.com>
2022-01-21 09:51:27 -0400

    actualizacion 2 heroku prod

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/paises-app-v1'));
+app.use(express.static(__dirname + '/dist/paisesApp'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/paises-app-v1/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/paisesApp/index.html'));
 });
 
 // default Heroku port
